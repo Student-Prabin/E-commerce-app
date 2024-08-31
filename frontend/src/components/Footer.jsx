6690bc4673ec0d4aef7dfbeba6f3d8b3fbeaf45d
Student-Prabin <dahalprabin74@gmail.com>
2024-08-31 12:28:19 -0700

    did some home page

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import { assets } from "../assets/assets"
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img className="mb-5 w-32" src={assets.logo} alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam, doloribus placeat explicabo doloremque corrupti facere nam unde exercitationem architecto ipsam odio molestias</p>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-grey-600">
+            <li>HOME</li>
+            <li>ABOUT US</li>
+            <li>DELIVERY</li>
+            <li>PRIVACY POLICY</li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-grey-600">
+            <li>+977 9849000012</li>
+            <li>contact.shopatus@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <hr />
+        <p className="py-y text-sm text-center">Copyright2024@ forever.com - All Rights Reserved</p>
+      </div>
+
+    </div>
+  )
+}
+export default Footer
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,20 @@
+import BestSeller from "../components/BestSeller"
+import Footer from "../components/Footer"
+import Hero from "../components/Hero"
+import LatestCollection from "../components/LatestCollection"
+import NewsletterBox from "../components/NewsletterBox"
+import OurPolicy from "../components/OurPolicy"
+
 const Home = () => {
   return (
-    <div>home</div>
+    <div>
+      <Hero />
+      <LatestCollection />
+      <BestSeller />
+      <OurPolicy />
+      <NewsletterBox />
+      <Footer />
+    </div>
   )
 }
 export default Home
