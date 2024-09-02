617630b7e0cf975611d3f67b980d6bedd7d375f5
Student-Prabin <dahalprabin74@gmail.com>
2024-09-01 21:13:54 -0700

    progress

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -12,9 +12,22 @@ const Collection = () => {
   const [category, setCategory] = useState([]);
   const [subCategory, setSubCategory] = useState([]);
 
+  const toggleCategory = () => {
+
+    if (category.includes(e.target.value)) {
+      setCategory(prev => prev.filter(item => item !== e.target.value))
+    } else {
+      setCategory(prev => [...prev, <e className="target valueonChange"></e>])
+    }
+  }
+
   useEffect(() => {
     setFilterProducts(products)
   }, [])
+
+  useEffect(() => {
+    console.log(category)
+  }, [category])
 
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
@@ -29,13 +42,13 @@ const Collection = () => {
           <p className="mb-3 text-sm font-medium">CATEGORIES</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
             <p className="flex gap-2">
-              <input className="w-3" type="checkbox" value={'Men'} />Men
+              <input className="w-3" type="checkbox" value={'Men'} onChange={toggleCategory} />Men
             </p>
             <p className="flex gap-2">
-              <input className="w-3" type="checkbox" value={'Women'} />Women
+              <input className="w-3" type="checkbox" value={'Women'} onChange={toggleCategory} />Women
             </p>
             <p className="flex gap-2">
-              <input className="w-3" type="checkbox" value={'Kids'} />Kids
+              <input className="w-3" type="checkbox" value={'Kids'} onChange={toggleCategory} />Kids
             </p>
           </div>
         </div>
