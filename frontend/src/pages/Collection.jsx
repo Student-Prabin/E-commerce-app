2106ec8b02c6b9a94c9c79051073bac4e7562ac1
Student-Prabin <dahalprabin74@gmail.com>
2024-09-02 21:06:49 -0700

    some changes

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -25,9 +25,9 @@ const Collection = () => {
     setFilterProducts(products)
   }, [])
 
-  useEffect(() => {
-    console.log(category)
-  }, [category])
+  // useEffect(() => {
+  //   console.log(category)
+  // }, [category])
 
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
