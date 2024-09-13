939c3d268c1f8b95df9e9d032786e635ae8835fb
Student-Prabin <dahalprabin74@gmail.com>
2024-09-12 22:53:05 -0700

    Add to cart

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -20,7 +20,7 @@ const RelatedProducts = ({ category, subCategory }) => {
   return (
     <div className="my-24">
       <div className="text-center text-3xl py-2">
-        <title text1={'RELATED'} Text2={'PRODUCTS'} ></title>
+        <title text1={'RELATED'} text2={'PRODUCTS'} ></title>
       </div>
 
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 ">{related.map((item, index) => (
