af91a0c8a2d570b6c3f17629a16142f79189d103
Student-Prabin <dahalprabin74@gmail.com>
2024-09-14 21:21:51 -0700

    add to cart feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,24 @@
-import React from "react"
-import { Routes, Route } from 'react-router-dom'
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Collection from "./pages/Collection"
-import Contact from "./pages/Contact"
-import Product from "./pages/Product"
-import Cart from "./pages/Cart"
-import Login from "./pages/Login"
-import PlaceOrder from "./pages/PlaceOrder"
-import Orders from "./pages/Orders"
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
-import SearchBar from "./components/SearchBar"
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Collection from "./pages/Collection";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import SearchBar from "./components/SearchBar";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <ToastContainer />
       <Navbar />
       <SearchBar />
       <Routes>
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
@@ -11,6 +12,11 @@ const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
 
   const addToCart = async (itemId, size) => {
+
+    if (!size) {
+      toast.error('Select product size');
+      return;
+    }
 
     let cartData = structuredClone(cartItems);
 
@@ -26,12 +32,25 @@ const ShopContextProvider = (props) => {
     }
     setCartItems(cartData);
   }
-  useEffect(() => {
-    console.log(cartItems);
-  }, [cartItems])
+
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
+          }
+        } catch (err) {
+
+        }
+      }
+    }
+    return totalCount;
+  }
 
   const value = {
-    products, currency, deliveryFee, search, setSearch, showSearch, setShowSearch, cartItems, addToCart
+    products, currency, deliveryFee, search, setSearch, showSearch, setShowSearch, cartItems, addToCart, getCartCount
   }
 
   return (
