71cee29c789e0184251cd48d4e234e905b6f50f4
Student-Prabin <dahalprabin74@gmail.com>
2024-09-16 16:56:07 -0700

    cart items

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -49,8 +49,14 @@ const ShopContextProvider = (props) => {
     return totalCount;
   }
 
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId][size] = quantity;
+    setCartItems(cartData);
+  }
+
   const value = {
-    products, currency, deliveryFee, search, setSearch, showSearch, setShowSearch, cartItems, addToCart, getCartCount
+    products, currency, deliveryFee, search, setSearch, showSearch, setShowSearch, cartItems, addToCart, getCartCount, updateQuantity
   }
 
   return (
